bcf9dfccb2c60289275b03290cc874ea56392a0d
Rikas <rikasrkf@gmail.com>
2024-10-10 01:12:36 +0530

    project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-black">
+        Task Management App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
